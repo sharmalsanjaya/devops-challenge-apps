8503f734efaaca894eb6afe8b700af44f3a93f50
Sharmal Sanjaya <sharmalsanjaya4@gmail.com>
2025-04-01 15:37:11 +0530

    Update app.js

--- web/app.js
+++ web/app.js
@@ -9,7 +9,7 @@ var routes = require('./routes/index');
 
 var app = express();
 
-// view engine setupp
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
